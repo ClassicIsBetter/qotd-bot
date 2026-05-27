026c3948c6d82c1d715f776ff0037bc996ead7e8
ClassicIsBetter <dexterbooth3@proton.me>
2026-05-27 17:43:39 +0930

    Create snakeConfig.js

---
+++ config/snakeConfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  size: 6,
+
+  emojis: {
+    empty: "⬛",
+    apple: "🍎",
+    body: "🟩",
+
+    head: {
+      up: "⬆️",
+      down: "⬇️",
+      left: "⬅️",
+      right: "➡️"
+    }
+  }
+
+};
